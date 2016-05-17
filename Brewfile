635db192611b5af1e5ad45df5cb04455bbf7a98f
Liane Nakamura <idiofox555@gmail.com>
2016-05-16 20:21:33 -0700

    Add Brewfile

---
+++ Brewfile
@@ -0,0 +1,18 @@
+cask_args appdir: '/Applications'
+
+tap 'homebrew/bundle'
+
+brew 'gradle'
+brew 'imagemagick'
+brew 'node'
+brew 'redis'
+brew 'swiftlint'
+brew 'go'
+brew 'pyenv'
+
+cask 'adium'
+cask 'google-chrome'
+cask 'dockertoolbox'
+cask 'imageoptim'
+cask 'postgres'
+cask 'sublime-text'
--- bootstrap.sh
+++ bootstrap.sh
@@ -5,7 +5,20 @@
 #-----------------------------------------------------------------------------
 
 # Notice title
-notice() { echo  "\033[1;32m=> $1\033[0m"; }
+
+info () {
+  printf "\r  [ \033[00;34m..\033[0m ] $1\n"
+}
+
+success () {
+  printf "\r\033[2K  [ \033[00;32mOK\033[0m ] $1\n"
+}
+
+fail () {
+  printf "\r\033[2K  [\033[0;31mFAIL\033[0m] $1\n"
+  echo ''
+  exit
+}
 
 backup() {
   mkdir -p $backupdir
@@ -43,44 +56,58 @@ in_array() {
 #-----------------------------------------------------------------------------
 
 backupdir="$HOME/.dotfiles-backup/$(date "+%Y%m%d%H%M.%S")"
-excluded=(. .. .git .gitignore bootstrap.sh README.md)
+excluded=(. .. .git .gitignore bootstrap.sh Brewfile README.md)
 
 
 #-----------------------------------------------------------------------------
 # Install
 #-----------------------------------------------------------------------------
 
-# Assumes $HOME/.dotfiles is *ours*
-if [ -d $HOME/.dotfiles ]; then
-  pushd $HOME/.dotfiles
+# Backup
+info "Backup up old files ($backupdir)"
+backup
 
-  # Update Repo
-  notice "Updating"
-  git pull origin master
-  git submodule init
-  git submodule update
+# Install
+info "Installing"
+install
 
-  # Backup
-  notice "Backup up old files ($backupdir)"
-  backup
 
-  # Install
-  notice "Installing"
-  install
-else
-  # Clone Repo
-  notice "Downloading"
-  git clone --recursive git://github.com/lnakamura/dotfiles.git $HOME/.dotfiles
+#-----------------------------------------------------------------------------
+# Homebrew
+#-----------------------------------------------------------------------------
 
-  pushd $HOME/.dotfiles
+if [ "$(uname -s)" == "Darwin" ]
+then
+  info "installing dependencies"
 
-  # Backup
-  notice "Backup up old files ($backupdir)"
-  backup
+  # Install and setup homebrew
+  if test ! $(which brew)
+  then
+    echo "  Installing Homebrew for you."
 
-  # Install
-  notice "Installing"
-  install
+    # Install the correct homebrew for each OS type
+    if test "$(uname)" = "Darwin"
+    then
+      ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    elif test "$(expr substr $(uname -s) 1 5)" = "Linux"
+    then
+      ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/linuxbrew/go/install)"
+    fi
+
+  fi
+
+  echo "› brew update"
+  brew update
+
+  # Run Homebrew through the Brewfile
+  echo "› brew bundle"
+  brew bundle
+
+  echo "› brew cleanup"
+  brew cleanup
+
+  success "dependencies installed"
+
 fi
 
 
@@ -88,6 +115,6 @@ fi
 # Finished
 #-----------------------------------------------------------------------------
 
-popd
-notice "Done"
+echo ''
+echo '  All installed!'
 exec $SHELL -l
